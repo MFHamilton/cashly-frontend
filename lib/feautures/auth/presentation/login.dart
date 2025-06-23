4c693d8272d6b1e83a5d733d952e487b580e6bb9
MFHamilton <mariahamiltonr1@gmail.com>
2025-06-22 20:17:05 -0400

    dont know what i fucked up

--- lib/feautures/auth/presentation/login.dart
+++ lib/feautures/auth/presentation/login.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
