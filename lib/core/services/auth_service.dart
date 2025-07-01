1b284e0c9d3a1527cde56113999db8e138f23eaf
fmadera <fmadera@cerberussolutions.io>
2025-07-01 09:17:17 -0400

    fix: usar user model para autenticacion

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,15 +1,26 @@
 import 'dart:convert';
 
+import 'package:cashly/core/models/user_model.dart';
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:http/http.dart' as http;
 
 Future<http.Response?> loginService(String email, String pass) async {
   // TODO: usar url correcta y mostrar mensaje adecuado
   try {
+    final user = User(
+      usuarioNombre: "",
+      usuarioApellido: "",
+      usuarioCorreo: email,
+      usuarioPassword: pass,
+      usuarioPais: "",
+      usuarioFechaNacimiento: DateTime.now(),
+    );
+
     var url = Uri.https('', '');
     var response = await http.post(
       url,
-      body: {'usuario_correo': email, 'usuario_password': pass},
+      body: user.toJson(),
+      headers: {"Content-Type": "application/json"},
     );
 
     if (response.statusCode == 200) {
@@ -26,33 +37,39 @@ Future<http.Response?> loginService(String email, String pass) async {
 }
 
 Future<http.Response?> registerService({
-  required String usuario_nombre,
-  required String usuario_apellido,
-  required String usuario_correo,
-  required String usuario_password,
-  required String usuario_pais,
-  String? usuario_moneda,
-  required DateTime usuario_fecha_nacimiento,
+  required String usuarioNombre,
+  required String usuarioApellido,
+  required String usuarioCorreo,
+  required String usuarioPassword,
+  required String usuarioPais,
+  String? usuarioMoneda,
+  required DateTime usuarioFechaNacimiento,
 }) async {
   // TODO: usar url correcta y mostrar mensaje adecuado
   try {
+    final user = User(
+      usuarioNombre: usuarioNombre,
+      usuarioApellido: usuarioApellido,
+      usuarioCorreo: usuarioCorreo,
+      usuarioPassword: usuarioPassword,
+      usuarioPais: usuarioPais,
+      usuarioFechaNacimiento: usuarioFechaNacimiento,
+    );
+
     var url = Uri.https('', '');
     var response = await http.post(
       url,
-      body: {
-        "usuario_nombre": usuario_nombre,
-        "usuario_apellido": usuario_apellido,
-        "usuario_correo": usuario_correo,
-        "usuario_password": usuario_password,
-        "usuario_pais": usuario_pais,
-        "usuario_moneda": usuario_moneda,
-        "usuario_fecha_nacimiento": usuario_fecha_nacimiento,
-      },
+      body: user.toJson(),
+      headers: {"Content-Type": "application/json"},
     );
 
     if (kDebugMode) print(jsonDecode(response.body));
 
-    return jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+
+    return null;
   } catch (e) {
     if (kDebugMode) {
       print("error: $e");
