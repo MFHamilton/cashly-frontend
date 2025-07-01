ee09398e922c4cc4ccab0550c06e0fe83478dee8
roldyk <roldyk.rojas@gmail.com>
2025-07-01 17:06:49 -0400

    FEAT: auth_service

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -3,10 +3,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-
-
-
-
 class AuthService {
   final _storage = const FlutterSecureStorage();
   final _baseUrl = "https://cashlyservice.onrender.com";
