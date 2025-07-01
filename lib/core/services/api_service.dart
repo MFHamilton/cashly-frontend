98f96e126e124801942941882035aa825b540f9c
roldyk <roldyk.rojas@gmail.com>
2025-07-01 17:10:49 -0400

    FEAT : api_service , initial logic programmed

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,1 +1,26 @@
-// capa genérica para hacer peticiones HTTP al backend
+import 'dart:convert';
+import 'package:cashly/core/models/categoria.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+
+
+class ApiService {
+  final _baseUrl = "https://cashlyservice.onrender.com";
+  final _storage = const FlutterSecureStorage();
+  final http.Client _client;
+
+  ApiService([http.Client? client]) : _client = client ?? http.Client();
+
+  Future<Map<String,String>> _authHeaders() async {
+    final token = await _storage.read(key: 'jwt');
+    if (token == null) throw Exception('Usuario no autenticado');
+    return {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+  }
+
+
+}
+
