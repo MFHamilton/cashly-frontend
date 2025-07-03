5b51c23352d00117c7610830572379dd973f2963
fmadera <fmadera@cerberussolutions.io>
2025-07-03 18:39:50 -0400

    feat: homescreen ya estructurado

--- lib/feautures/home_screen.dart
+++ lib/feautures/home_screen.dart
@@ -54,53 +54,59 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                childAspectRatio: 3 / 2,
-                children: [
-                  CustomCard(
-                    titleIcon: Icons.arrow_circle_up_sharp,
-                    cardTitle: "Ingresos",
-                    amount: 30000,
-                    bottomContent: "+5% al mes anterior",
-                  ),
+            GridView.count(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                CustomCard(
+                  titleIcon: Icons.arrow_circle_up_sharp,
+                  cardTitle: "Ingresos",
+                  amount: 30000,
+                  bottomContent: "+5% al mes anterior",
+                ),
 
-                  CustomCard(
-                    titleIcon: Icons.account_circle_outlined,
-                    cardTitle: "Gastos",
-                    amount: 30000,
-                    bottomContent: "+5% al mes anterior",
-                  ),
+                CustomCard(
+                  titleIcon: Icons.account_circle_outlined,
+                  cardTitle: "Gastos",
+                  amount: 30000,
+                  bottomContent: "+5% al mes anterior",
+                ),
 
-                  CustomCard(
-                    titleIcon: Icons.account_circle_outlined,
-                    cardTitle: "Presupuesto",
-                    amount: 30000,
-                    bottomContent: "+5% al mes anterior",
-                  ),
+                CustomCard(
+                  titleIcon: Icons.account_circle_outlined,
+                  cardTitle: "Presupuesto",
+                  amount: 30000,
+                  bottomContent: "+5% al mes anterior",
+                ),
 
-                  CustomCard(
-                    titleIcon: Icons.account_circle_outlined,
-                    cardTitle: "Metas",
-                    amount: 30000,
-                    bottomContent: "+5% al mes anterior",
-                  ),
-                ],
-              ),
+                CustomCard(
+                  titleIcon: Icons.account_circle_outlined,
+                  cardTitle: "Metas",
+                  amount: 30000,
+                  bottomContent: "+5% al mes anterior",
+                ),
+              ],
             ),
             // TODO: alertas
-            // TODO: grafica de comparativa mensual
+            // Gráfica de comparativa mensual
             Text(
               "Comparativa Mensual",
               style: MyTextTheme.lightTextTheme.titleLarge,
             ),
             MonthlyBarChart(),
             // TODO: fuente de ingresos
+            SizedBox(height: 20),
+            Text(
+              "Fuentes de Ingresos",
+              style: MyTextTheme.lightTextTheme.titleLarge,
+            ),
+            IncomeList(),
           ],
         ),
       ),
@@ -192,7 +198,7 @@ class MonthlyBarChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const barColor = Color(0xFF003C1F); // Verde oscuro
+    const barColor = AppColors.primary; // Verde oscuro
 
     final data = [
       {'day': '21/03', 'amount': 50.0},
@@ -205,62 +211,158 @@ class MonthlyBarChart extends StatelessWidget {
     ];
 
     return SizedBox(
-      height: 220,
-      child: BarChart(
-        BarChartData(
-          maxY: 120,
-          minY: 0,
-          alignment: BarChartAlignment.spaceAround,
-          gridData: FlGridData(show: false),
-          borderData: FlBorderData(show: false),
-          barTouchData: BarTouchData(enabled: false),
-          titlesData: FlTitlesData(
-            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      data[value.toInt()]['day'].toString(),
-                      style: const TextStyle(fontSize: 10),
+      height: 250,
+      child: Stack(
+        children: [
+          BarChart(
+            BarChartData(
+              maxY: 120,
+              minY: 0,
+              alignment: BarChartAlignment.spaceAround,
+              gridData: FlGridData(show: false),
+              borderData: FlBorderData(show: false),
+              barTouchData: BarTouchData(enabled: false),
+              titlesData: FlTitlesData(
+                topTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+                leftTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+                rightTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    getTitlesWidget: (value, meta) {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          data[value.toInt()]['day'].toString(),
+                          style: const TextStyle(fontSize: 10),
+                        ),
+                      );
+                    },
+                    reservedSize: 30,
+                  ),
+                ),
+              ),
+              barGroups: List.generate(data.length, (index) {
+                final item = data[index];
+                final value = item['amount'] as double;
+                return BarChartGroupData(
+                  x: index,
+                  barRods: [
+                    BarChartRodData(
+                      toY: value,
+                      color: barColor,
+                      width: 16,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(6),
+                        topRight: Radius.circular(6),
+                      ),
                     ),
-                  );
-                },
-                reservedSize: 30,
+                  ],
+                );
+              }),
+              groupsSpace: 12,
+            ),
+            swapAnimationDuration: const Duration(milliseconds: 500),
+            swapAnimationCurve: Curves.easeOut,
+          ),
+          // Añadir valores sobre las barras con Positioned widgets
+          Positioned.fill(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children:
+                    data.map((item) {
+                      return Expanded(
+                        child: Align(
+                          alignment: Alignment.topCenter,
+                          child: Text(
+                            '\$${item['amount']!.toString()}',
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF003C1F),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
               ),
             ),
           ),
-          barGroups: List.generate(data.length, (index) {
-            final item = data[index];
-            final value = item['amount'] as double;
-            return BarChartGroupData(
-              x: index,
-              barRods: [
-                BarChartRodData(
-                  toY: value,
-                  color: barColor,
-                  width: 16,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(6),
-                    topRight: Radius.circular(6),
-                  ),
-                  rodStackItems: [],
-                ),
-              ],
-              showingTooltipIndicators: [],
-              barsSpace: 8,
-            );
-          }),
-          extraLinesData: ExtraLinesData(horizontalLines: []),
-          // barTouchTooltipData: BarTouchTooltipData(enabled: false),
-          groupsSpace: 12,
+        ],
+      ),
+    );
+  }
+}
+
+class IncomeList extends StatelessWidget {
+  IncomeList({super.key});
+
+  final data = [
+    {"frecuencia": "Mensual", "fecha": "12-06-2025", "amount": 30000},
+    {"frecuencia": "Mensual", "fecha": "12-06-2025", "amount": 30000},
+    {"frecuencia": "Mensual", "fecha": "12-06-2025", "amount": 30000},
+    {"frecuencia": "Mensual", "fecha": "12-06-2025", "amount": 30000},
+    {"frecuencia": "Mensual", "fecha": "12-06-2025", "amount": 30000},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        final item = data[index];
+        return IncomeCard(
+          frequency: item["frecuencia"].toString(),
+          date: item["fecha"].toString(),
+          amount: double.parse(item["amount"].toString()),
+        );
+      },
+    );
+  }
+}
+
+class IncomeCard extends StatelessWidget {
+  const IncomeCard({
+    super.key,
+    required this.frequency,
+    required this.date,
+    required this.amount,
+  });
+
+  final String frequency;
+  final String date;
+  final double amount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8), // Bordes redondeados
+      ),
+      color: Colors.grey[300],
+      elevation: 4,
+      margin: EdgeInsets.all(8),
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: Row(
+          children: [
+            // TODO: salario mensual y fecha
+            Column(children: [Text("Salario $frequency"), Text(date)]),
+            Spacer(),
+            // TODO: cantidad
+            Column(children: [Text("+RD\$$amount"), Text("Salario")]),
+          ],
         ),
-        swapAnimationDuration: const Duration(milliseconds: 500),
-        swapAnimationCurve: Curves.easeOut,
       ),
     );
   }
