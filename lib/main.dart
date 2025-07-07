5aa171ef33614f2d5cab77cdf6804d44fccbd6fc
MFHamilton <mariahamiltonr1@gmail.com>
2025-07-07 08:59:47 -0400

    google auth code inserted

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:cashly/core/themes/app_themes.dart';
 import 'package:cashly/feautures/intro/presentation/intro_page.dart';
 import 'package:cashly/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
+  await dotenv.load();
 
   runApp(MyApp());
 
