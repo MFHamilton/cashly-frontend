009bd418a7b278a200b6418b3966cb13ffa68846
fmadera <fmadera@cerberussolutions.io>
2025-07-09 18:21:36 -0400

    chore: agregar meta

--- lib/feautures/goals/presentation/add_goal.dart
+++ lib/feautures/goals/presentation/add_goal.dart
@@ -1,6 +1,7 @@
 import 'package:cashly/core/constants/app_color.dart';
 import 'package:cashly/core/themes/text_scheme.dart';
 import 'package:cashly/core/widgets/custom_button.dart';
+import 'package:cashly/core/widgets/form_input.dart';
 import 'package:cashly/core/widgets/header.dart';
 import 'package:cashly/core/widgets/input.dart';
 import 'package:flutter/foundation.dart';
@@ -46,34 +47,30 @@ class AddGoalScreen extends StatelessWidget {
                 ],
               ),
             ),
-            GoalInput(
-              controller: nameController,
-              label: "Nombre de la meta",
-              count: true,
-              defaultText: "ej: fondo de emergencias",
+            FormInput(
+              inputController: nameController,
+              title: "Nombre de la meta",
+              hintText: "ej: fondo de emergencias",
               icon: Icons.label,
             ),
-            SizedBox(height: 20),
-            GoalInput(
-              controller: amountController,
-              label: "Monto",
-              count: true,
-              defaultText: "describe tu meta..",
-              icon: Icons.attach_money,
+            FormInput(
+              inputController: nameController,
+              title: "Nombre de la meta",
+              hintText: "ej: fondo de emergencias",
+              icon: Icons.label,
             ),
-            SizedBox(height: 20),
-            GoalInput(
-              controller: descriptionController,
-              label: "Descripción",
-              count: true,
-              defaultText: "ej: fondo de emergencias",
+            FormInput(
+              inputController: nameController,
+              title: "Nombre de la meta",
+              hintText: "ej: fondo de emergencias",
+              icon: Icons.label,
             ),
             // TODO: seleccionar categoria
-            GoalInput(controller: controller, label: label, count: count, defaultText: defaultText),
+            // GoalInput(controller: controller, label: label, count: count, defaultText: defaultText),
             // TODO: fechas
             // boton para enviar la solicitud
             Padding(
-              padding: EdgeInsets.fromLTRB(8, 16, 8, 8),
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
               child: CustomButton(
                 text: "Guardar Meta",
                 onPressed: () {
@@ -83,101 +80,6 @@ class AddGoalScreen extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class GoalInput extends StatefulWidget {
-  const GoalInput({
-    super.key,
-    required this.controller,
-    required this.label,
-    required this.count,
-    required this.defaultText,
-    this.icon,
-  });
-
-  final TextEditingController controller;
-  final String label;
-  final bool count;
-  final String defaultText;
-  final IconData? icon;
-
-  @override
-  State<GoalInput> createState() => _GoalInputState();
-}
-
-class _GoalInputState extends State<GoalInput> {
-  int _charCount = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.count) {
-      widget.controller.addListener(() {
-        setState(() {
-          _charCount = widget.controller.text.length;
-        });
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: AppColors.textPrimary, width: 1),
-        borderRadius: BorderRadius.circular(4),
-      ),
-      padding: EdgeInsets.all(12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              if (widget.icon != null)
-                Icon(
-                  widget.icon,
-                  size: 20,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              SizedBox(width: 6),
-              Text(
-                'Nombre de la Meta',
-                style: Theme.of(
-                  context,
-                ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          TextFormField(
-            controller: widget.controller,
-            maxLength: 20,
-            decoration: InputDecoration(
-              hintText: 'ej: fondo de emergencia',
-              counterText: (widget.count) ? '$_charCount/20 caracteres' : '',
-              filled: true,
-              fillColor: Theme.of(context).colorScheme.surface,
-              contentPadding: const EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 12,
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(3),
-                borderSide: BorderSide(
-                  color:
-                      Colors
-                          .grey
-                          .shade300, // o cualquier color que coincida con tu diseño
-                  width: 1,
-                ),
-              ),
-            ),
-            style: Theme.of(context).textTheme.bodySmall,
-          ),
-        ],
       ),
     );
   }
