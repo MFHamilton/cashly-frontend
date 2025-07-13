cd6a6ba5f8fd60a1115c44fd0f707f7552ab239b
MFHamilton <mariahamiltonr1@gmail.com>
2025-07-13 17:21:40 -0400

    delete message created

--- lib/feautures/test/testingPage.dart
+++ lib/feautures/test/testingPage.dart
@@ -1,5 +1,7 @@
+import 'package:cashly/core/models/ingreso.dart';
 import 'package:cashly/core/widgets/category.dart';
 import 'package:cashly/core/widgets/confirmation_message.dart';
+import 'package:cashly/core/widgets/delete_message.dart';
 import 'package:cashly/core/widgets/form_input.dart';
 import 'package:cashly/core/widgets/frecuency.dart';
 import 'package:cashly/core/widgets/duration.dart';
@@ -13,9 +15,9 @@ class _TestingPageState extends State<TestingPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
-        child: ConfirmationMessage(
-          controllerName: 'Gastos',
-          targetRoute: GastosScreen(),),
+        child: DeleteMessage(
+            controllerName: 'ingreso',
+            targetRoute: GastosScreen()),
 
       ),
     );
