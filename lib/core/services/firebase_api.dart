3c02b81d55ebee88d1f673d98a8b0d1b734942d3
MFHamilton <mariahamiltonr1@gmail.com>
2025-07-16 00:01:20 -0400

    feat: local notifs working

--- lib/core/services/firebase_api.dart
+++ lib/core/services/firebase_api.dart
@@ -1,8 +1,11 @@
 
+import 'dart:convert';
+
 import 'package:cashly/feautures/budget/presentation/budget.dart';
 import 'package:cashly/feautures/test/testingPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import '../../main.dart';
 
@@ -20,6 +23,16 @@ class FirebaseApi{
 
   final _firebaseMessaging = FirebaseMessaging.instance;
 
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.defaultImportance,
+
+  );
+
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
 
 
   void handleMessage(RemoteMessage? message) {
@@ -29,6 +42,28 @@ class FirebaseApi{
       TestingPage.route,
       arguments: message,
     );
+
+  }
+
+  Future initLocalNotifications() async{
+    const android = AndroidInitializationSettings('@drawable/ic_launcher');
+    const settings = InitializationSettings(android: android);
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) {
+        final payload = response.payload;
+        if (payload != null) {
+          final message = RemoteMessage.fromMap(jsonDecode(payload));
+          handleMessage(message);
+        }
+      },
+    );
+
+    final platform = _localNotifications.resolvePlatformSpecificImplementation
+    <AndroidFlutterLocalNotificationsPlugin>();
+
+    await platform?.createNotificationChannel(_androidChannel);
 
   }
 
@@ -43,6 +78,26 @@ class FirebaseApi{
     FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+
+      _localNotifications.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@drawable/ic_launcher',
+          ),
+        ),
+        payload: jsonEncode(message.toMap()),
+
+      );
+    });
 
   }
 
@@ -55,5 +110,6 @@ class FirebaseApi{
     final token = await _firebaseMessaging.getToken();
     print('Token: $token');
     initPushNotifications();
+    initLocalNotifications();
   }
 }
