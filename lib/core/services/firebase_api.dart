9d6a28845b9a5744e635a01f4ca1b6ca7c0b9e94
MFHamilton <mariahamiltonr1@gmail.com>
2025-07-15 22:49:18 -0400

    fix: background push notfs working

--- lib/core/services/firebase_api.dart
+++ lib/core/services/firebase_api.dart
@@ -1,13 +1,59 @@
 
+import 'package:cashly/feautures/budget/presentation/budget.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+import '../../main.dart';
+
+@pragma('vm:entry-point')
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('🔔 Background handler');
+  print('Title: ${message.notification?.title}');
+  print('Body: ${message.notification?.body}');
+  print('Payload: ${message.data}');
+}
+
+
 class FirebaseApi{
+
   final _firebaseMessaging = FirebaseMessaging.instance;
+
+
+  /*
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) return;
+
+    navigatorKey.currentState?.pushNamed(
+      BudgetScreen.route,
+      arguments: message,
+    );
+
+  }
+
+
+  Future initPushNotifications() async{
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
+  }
+
+   */
+
+
 
   Future<void> initNotification() async {
     await _firebaseMessaging.requestPermission();
     final token = await _firebaseMessaging.getToken();
     print('Token: $token');
-
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    //initPushNotifications();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:cashly/core/themes/app_themes.dart';
 import 'package:cashly/feautures/auth/presentation/login.dart';
+import 'package:cashly/feautures/budget/presentation/budget.dart';
 import 'package:cashly/feautures/gastos/presentation/gastos_screen.dart';
 import 'package:cashly/feautures/goals/presentation/add_goal.dart';
 import 'package:cashly/feautures/home/presentation/home_screen.dart';
 import 'package:cashly/feautures/intro/presentation/intro_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'core/services/firebase_api.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   await FirebaseApi().initNotification();
 
   //await dotenv.load(); // Cargando las variables de entorno
@@ -33,7 +37,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
      theme: AppTheme.lightTheme,
-      home: LoginPage(),
+      home: HomeScreen(),
+      /*
+      routes: {
+        BudgetScreen.route: (context) => BudgetScreen(),
+      },
+
+       */
     );
   }
 }
